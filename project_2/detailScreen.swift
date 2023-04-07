8ba36ab597fbecc01a46c6d6b0bdb9daf00e42fa
sagargrg28 <grgsagar28@gmail.com>
2023-04-07 18:18:05 -0400

    weather data for the 7 days displayed in the table

--- project_2/detailScreen.swift
+++ project_2/detailScreen.swift
@@ -11,12 +11,16 @@ import CoreLocation
 class detailScreen: UIViewController {
     var longitude: Double?
     var latitude: Double?
-    
+    var fullLocation: String?
+    private var items: [tableItem] = []
+    @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var lowTemp: UILabel!
     @IBOutlet weak var highTemp: UILabel!
     @IBOutlet weak var weather: UILabel!
     @IBOutlet weak var temp_C: UILabel!
     @IBOutlet weak var location: UILabel!
+    
+  
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -25,18 +29,22 @@ class detailScreen: UIViewController {
         print(latitude!)
         
         if let latitude = latitude, let longitude = longitude{
-            let fullLocation = "\(latitude),\(longitude)"
-            loadWeather(search: fullLocation)
+            fullLocation = "\(latitude),\(longitude)"
+            loadWeather(search: fullLocation, days: 1)
         }
+        
+        loadTableItems()
+        tableView.dataSource = self
+        
         
     }
     
-    private func loadWeather(search: String?){
+    private func loadWeather(search: String?, days: Int){
         guard let search = search else{
             return
         }
         //Get url
-        guard let url = getURl(query: search) else{
+        guard let url = getURl(query: search, days: days) else{
             print("URL not found ")
             return
         }
@@ -56,9 +64,7 @@ class detailScreen: UIViewController {
             }
             
             if let WeatherData = self.parseJson(data: data){
-                
-                print(WeatherData.current.condition.text)
-                print(WeatherData.location.name)
+         
                 for forecastDay in WeatherData.forecast.forecastday {
                     print (forecastDay.day.maxtemp_c)
                     print (forecastDay.day.mintemp_c)
@@ -95,19 +101,110 @@ class detailScreen: UIViewController {
         
     }
     // Making URL
-    private func getURl(query: String)->URL?{
+    private func getURl(query: String, days: Int)->URL?{
         let baseUrl = "https://api.weatherapi.com/v1/"
         let endPoint = "forecast.json"
-        let apiKey = "03b553e6eb96411a8ef21900231603"
-        guard let url = "\(baseUrl)\(endPoint)?key=\(apiKey)&q=\(query)&days=1&aqi=no&alerts=no".addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed) else{
+        let apiKey = "60acdd71266c48b2a8a193311230704"
+        guard let url = "\(baseUrl)\(endPoint)?key=\(apiKey)&q=\(query)&days=\(days)&aqi=no&alerts=no".addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed) else{
             return nil
         }
         return URL(string: url)
     }
     
+    private func loadTableItems(){
+        
+        //Get url
+        guard let url = getURl(query: fullLocation!, days: 7) else{
+            print("URL not found ")
+            return
+        }
+        //create url session
+        let urlSession = URLSession.shared
+        //creating task for session
+        let dataTask = urlSession.dataTask(with: url) { data, response, error in
+            
+            guard  error == nil else {
+                print("Error received ")
+                return
+            }
+            
+            guard let data = data else {
+                print ("Data not received ")
+                return
+            }
+            
+            if let WeatherData = self.parseJson(data: data){
+         
+                for forecastDay in WeatherData.forecast.forecastday {
+                    print (forecastDay.day.maxtemp_c)
+                  
+                   
+                }
+                
+                DispatchQueue.main.async {
+                    for forecastDay in WeatherData.forecast.forecastday{
+                        self.items.append(tableItem(day: forecastDay.date, temperature: forecastDay.day.avgtemp_c, icon: UIImage(systemName: "\(self.weatherImage(code: forecastDay.day.condition.code))")))
+                        self.tableView.reloadData()
+
+                    }
+                    
+                }
+            }
+            
+        }
+        //start the task
+        dataTask.resume()
+        
+       
+
+    }
+    
+    //Function to load the icon string according to the weather code
+    private func weatherImage(code: Int)-> String{
+        
+        var image = ""
+        switch code{
+        case 1000 :  image = "sun.max"
+        case 1006, 1003 :   image = "cloud.fill"
+        case 1009 : image = "cloud"
+        case 1030, 1135, 1147 : image = "cloud.fog.fill"
+        case 1063, 1180, 1183, 1186, 1189, 1192, 1195, 1198, 1201,1240,1243, 1246, 1249 : image = "cloud.rain.fill"
+            
+        case 1087 : image = "cloud.bolt.rain"
+        case 1114, 1210,1213,1216,1219,1222,1225,1255,1258,1279,1282 : image = "cloud.snow.fill"
+        default : image = "cloud.sun.bolt.fill"
+        }
+        return image
+    }
     
     
     
+    
+}
+
+extension detailScreen: UITableViewDataSource{
+    public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return items.count
+    }
+    
+    public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "toDoCell", for: indexPath)
+        let item = items[indexPath.row]
+        var content = cell.defaultContentConfiguration()
+        content.text = item.day
+        content.secondaryText = "\(item.temperature)"
+        content.image = item.icon
+        cell.contentConfiguration = content
+        return cell
+    }
+    
+    
+}
+
+struct tableItem{
+    let day: String
+    let temperature: Float
+    let icon: UIImage?
 }
 
 struct WeatherData: Decodable {
